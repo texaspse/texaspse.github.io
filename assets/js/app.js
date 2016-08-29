6b600a0a0c00832ea87a7c1a9fa08ccf66c3b240
texaspse <texaspse@gmail.com>
2016-08-29 17:30:44 -0500

    Created main index page
    Includes description, what we do, what we cover, and customized google
    form to keep track of membership.

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* Otherwise just put the config content (json): */
+
+particlesJS.load('particles-js', 'assets/particlesjs-pse.json', function() {
+  console.log('callback - particles.js config loaded');
+});
